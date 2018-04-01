2fc8f58930e21375fe67036d712230fd51242a02
davidnotplay <dcasadomartinez@gmail.com>
2018-04-01 03:24:28 +0200

    Tested in React 15.6

--- src/__test__/utils.js
+++ src/__test__/utils.js
@@ -1,4 +1,13 @@
+import React from 'react'
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 
-configure({adapter: new Adapter() })
+const versionNumber =  React.version.split('.')
+let adapter = null
+
+if (versionNumber[0] == '16') {
+  adapter = new (require('enzyme-adapter-react-16'))
+} else {
+  adapter = new (require('enzyme-adapter-react-15'))
+}
+
+configure({ adapter })
